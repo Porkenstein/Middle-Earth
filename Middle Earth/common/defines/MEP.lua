e8f01bdb61d6c33dda86552f12f1ed159ac278bc
Cody Lee Collins <xpertgamer87@gmail.com>
2019-04-02 11:30:28 -0400

    Think I got all edits over.

--- Middle Earth/common/defines/MEP.lua
+++ Middle Earth/common/defines/MEP.lua
@@ -1,6 +1,21 @@
 -- Format for overwriting define values:
 --
 -- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+NDefines.NDiplomacy.BASE_REVOLT_CHANCE_MOD = 125
+NDefines.NDiplomacy.TOG_REVOLT_CHANCE_MOD = 50
+
 NDefines.NCharacter.PORTRAIT_MID_AGE_THRESHOLD = 55
 NDefines.NCharacter.PORTRAIT_OLD_AGE_THRESHOLD = 120
 NDefines.NCharacter.MAX_CHILD_BIRTH_AGE = 12000
+NDefines.NCharacter.TREASURY_CHANCE_TO_DISAPPEAR_STANDARD = 0.05
+
+NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 1,				-- Set this to your earliest starting year
+NDefines.NTechnology.POINTS_PER_ATTRIBUTE = 0.00,
+NDefines.NTechnology.BASE_NEIGHBOUR_SPREAD_BONUS = 0.00, 		-- bonus for each neighbour with the tech
+NDefines.NTechnology.NEIGHBOUR_SAME_RELIGON_GROUP_MULT = 2.0, 	-- multiplier to the above bonus if neighbour is of your religious group
+NDefines.NTechnology.BASE_DEMESNE_SPREAD_BONUS = 0.00, 			-- bonus for each other province in demesne with the tech
+NDefines.NTechnology.MAX_DEMESNE_BONUS = 0.0, 					-- maximum bonus from demesne
+NDefines.NTechnology.TRADEPOST_SPREAD_BONUS = 0.000, 			-- Spread from trade posts
+NDefines.NTechnology.SPYACTION_SPREAD_BONUS = 0.000,				-- Spread from the target of the study technology spymaster action
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_0 = 1, 					-- the chance to get a progress to this level will increase after this date and decrease before it
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_8 = 16000
