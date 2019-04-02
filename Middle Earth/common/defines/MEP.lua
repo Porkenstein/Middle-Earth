006b0c09177d37aa34877ce7678b1174b64bf776
Cody Lee Collins <xpertgamer87@gmail.com>
2019-04-02 11:24:15 -0400

    Update removing defines file and opening common folder in .mod

---
+++ Middle Earth/common/defines/MEP.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+NDefines.NCharacter.PORTRAIT_MID_AGE_THRESHOLD = 55
+NDefines.NCharacter.PORTRAIT_OLD_AGE_THRESHOLD = 120
+NDefines.NCharacter.MAX_CHILD_BIRTH_AGE = 12000
